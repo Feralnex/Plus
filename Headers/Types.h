3cddca99c7024e2a978b33b12bb68b6e54948e92
Tomasz Czech <feralnex@gmail.com>
2025-10-20 20:30:13 +0200

    Changed Source folder name to Headers

---
+++ Headers/Types.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifdef _WIN32
+#include <vcruntime.h>
+#elif __linux__
+#include <sys/types.h>
+#endif
+
+#include <cstddef>
+
+#ifdef _WIN32
+typedef size_t Size;
+typedef ptrdiff_t SignedSize;
+#elif __linux__
+typedef size_t Size;
+typedef ssize_t SignedSize;
+#endif
+typedef char Byte;
